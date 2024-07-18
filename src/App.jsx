07c77cd2be3d232d8af3e2f7f5f9a70fc2812907
Megha Prajapati <mp348730@gmail.com>
2024-07-18 17:59:56 +0530

    Jobs page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import HomePage from '../pages/HomePage'
+import JobsPage from '../pages/JobsPage'
+import NotFoundPage from '../pages/NotFoundPage'
 import MainLayout from './layouts/MainLayout'
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 
@@ -7,6 +9,8 @@ const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/' element={<MainLayout/>}>
 			<Route index element={<HomePage/>}/>
+			<Route path='/jobs' element={<JobsPage/>}/>
+			<Route path='/*' element={<NotFoundPage/>}/>
 		</Route>
 	)
 )
