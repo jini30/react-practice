9c5e69338d4901beb9a96c0b33bf99dc59bc720e
Megha Prajapati <mp348730@gmail.com>
2024-07-18 18:54:01 +0530

    Json server implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomePage from '../pages/HomePage'
 import JobsPage from '../pages/JobsPage'
+import JobPage from '../pages/JobPage'
 import NotFoundPage from '../pages/NotFoundPage'
 import MainLayout from './layouts/MainLayout'
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
@@ -10,6 +11,7 @@ const router = createBrowserRouter(
 		<Route path='/' element={<MainLayout/>}>
 			<Route index element={<HomePage/>}/>
 			<Route path='/jobs' element={<JobsPage/>}/>
+			<Route path='/jobs/:id' element={<JobPage/>}/>
 			<Route path='/*' element={<NotFoundPage/>}/>
 		</Route>
 	)
